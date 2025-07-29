41b1bf0e798ca37f773c13e6afbd028f41ccfbe3
Sofie Budman <changbudmanfamily@gmail.com>
2025-07-29 11:06:38 -0400

    Russian Preprocess

---
+++ backend/src/multiclass_preprocess.py
@@ -0,0 +1,29 @@
+import io
+import cv2 
+import librosa
+import numpy as np
+def russain_preprocess_audio(file):
+    audio_bytes = file.read()
+    audio_buffer = io.BytesIO(audio_bytes) #read audio
+
+
+    y, sr = librosa.load(audio_buffer, sr=None)
+
+    S = librosa.stft(y, n_fft=1024, hop_length=512)
+    S_db = librosa.amplitude_to_db(np.abs(S), ref=np.max)
+
+    S_normalized = 255 * (S_db - S_db.min()) / (S_db.max() - S_db.min())
+    S_normalized = S_normalized.astype(np.uint8)
+
+
+    S_color = cv2.applyColorMap(S_normalized, cv2.COLORMAP_VIRIDIS)  # Shape: (freq, time, 3), dtype: uint8
+
+
+    S_resized = cv2.resize(S_color, (128, 128), interpolation=cv2.INTER_LINEAR)
+
+
+    S_resized = S_resized.astype(np.float32) / 255.0
+
+    S_final = np.expand_dims(  S_resized, axis=0)
+
+    return S_final
